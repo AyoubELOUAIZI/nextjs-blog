784465710e63e0d978fc030b9d14f3121288bf0b
AyoubELOUAIZI <ayoubelouaizi@gmail.com>
2023-10-22 12:08:17 +0100

    this code have some issues with system

---
+++ src/app/api/tags/route.ts
@@ -0,0 +1,12 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const tags = await db.tag.findFirst();
+    return NextResponse.json(tags, { status: 200 });
+  } catch (error:any) {
+    console.error('Error fetching tags:', error);
+    return NextResponse.json({ message: 'Could not fetch tags', error: error.message }, { status: 500 });
+  }
+}
